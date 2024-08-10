881d96e766d8f5732a6acb7c479d92e20cea4d97
fizasarfraz24001 <fizasarfraz24001@gmail.com>
2024-08-10 15:24:51 +0500

    nextjs_2nd_assigment

---
+++ src/app/homepage/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function homepage() {
+  return (
+    <div className="font-serif font-bold text-center"> My First Homepage</div>
+  )
+}
+
+export default homepage
---
+++ src/app/namepage/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function namepage() {
+  return (
+    <div className="font-serif font-bold text-center"> My FIrst Namepage</div>
+  )
+}
+
+export default namepage
